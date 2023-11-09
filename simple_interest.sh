1faa9593b933323b2d21ea79250e46b63fc451c8
Mrimmoy Haque Shimanto <140089835+codejutsu-desu@users.noreply.github.com>
2023-11-09 12:21:37 +0600

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Author: codejutsu-desu
+
+# This script calculates simple interest.
+
+# Prompt for principal amount, rate, and time.
+read -p "Enter principal amount: " principal
+read -p "Enter annual interest rate (as a decimal): " rate
+read -p "Enter time (in years): " time
+
+# Calculate simple interest.
+simple_interest=$(echo "scale=2; $principal * $rate * $time" | bc)
+
+# Display the result.
+echo "Simple Interest: $simple_interest"
 
